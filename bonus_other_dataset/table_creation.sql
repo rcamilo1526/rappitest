61fa9bcc736a6dc6c3efb3101aad6cf728a34d5c
Raúl Camilo Martín Bernal <rcmartinb@correo.udistrital.edu.co>
2021-09-04 22:43:18 -0500

    bonus. other dataset table creation

---
+++ bonus_other_dataset/table_creation.sql
@@ -0,0 +1,134 @@
+
+create table "ADVENTUREWORKS"."PUBLIC"."CountryRegionCurrency"(
+    countryregioncode text,
+    currencycode text,
+    modifiedtimestamp timestamp
+);
+
+
+create table "ADVENTUREWORKS"."PUBLIC"."CurrencyRate"(
+    currencyrateid  numeric,
+    currencyratetimestamp timestamp,
+    fromcurrencycode text,
+    tocurrencycode text,
+    averagerate float,
+    endofdayrate float,
+    modifiedtimestamp timestamp
+);
+
+create table "ADVENTUREWORKS"."PUBLIC"."Product"(
+    productid text primary key,
+    name text,
+    productnumber text,
+    makeflag text,
+    finishedgoodsflag text,
+    color text,
+    safetystocklevel numeric,
+    reorderponumeric numeric,
+    standardcost float,
+    listprice float,
+    size text,
+    sizeunitmeasurecode text,
+    weightunitmeasurecode text,
+    weight float,
+    daystomanufacture numeric,
+    productline text,
+    class text,
+    style text,
+    productsubcategoryid numeric,
+    productmodelid numeric,
+    sellstarttimestamp timestamp,
+    sellendtimestamp timestamp,
+    discontinuedtimestamp timestamp,
+    rowguid text,
+    modifiedtimestamp timestamp
+);
+
+create table "ADVENTUREWORKS"."PUBLIC"."ProductCategory"(
+    productcategoryid numeric primary key,
+    name text,
+    rowguid text,
+    modifiedtimestamp timestamp
+);
+create table "ADVENTUREWORKS"."PUBLIC"."ProductDescription"(
+    productdescriptionid numeric primary key,
+    description text,
+    rowguid text,
+    modifiedtimestamp timestamp
+);
+create table "ADVENTUREWORKS"."PUBLIC"."ProductModelProductDescriptionCulture"(
+    productmodelid numeric,
+    productdescriptionid numeric,
+    cultureid text,
+    modifiedtimestamp timestamp
+);
+
+create table "ADVENTUREWORKS"."PUBLIC"."ProductSubCategory"(
+    productsubcategoryid numeric primary key,
+    productcategoryid numeric,
+    name text,
+    rowguid text,
+    modifiedtimestamp timestamp
+);
+create table "ADVENTUREWORKS"."PUBLIC"."salesorderdetail"(
+    salesorderid numeric,
+    salesorderdetailid numeric,
+    carriertrackingnumber text,
+    orderqty numeric,
+    productid numeric,
+    specialofferid numeric,
+    unitprice float,
+    unitpricediscount float,
+    rowguid text,
+    modifiedtimestamp timestamp
+);
+create table "ADVENTUREWORKS"."PUBLIC"."SalesOrderHeader"(
+    salesorderid numeric primary key,
+    revisionnumber numeric,
+    ordertimestamp timestamp,
+    duetimestamp timestamp,
+    shiptimestamp timestamp,
+    status numeric,
+    onlineorderflag  text,
+    purchaseordernumber text,
+    accountnumber text,
+    customerid numeric,
+    salespersonid numeric,
+    territoryid numeric,
+    billtoaddressid numeric,
+    shiptoaddressid  numeric,
+    shipmethodid  numeric,
+    creditcardid  numeric,
+    creditcardapprovalcode text,
+    currencyrateid numeric,
+    subtotal  float,
+    taxamt float,
+    freight  float,
+    totaldue float,
+    comment text,
+    rowguid text,
+    modifiedtimestamp timestamp
+);
+create table "ADVENTUREWORKS"."PUBLIC"."salesperson"(
+    businessentityid numeric,
+    territoryid numeric,
+    salesquota numeric,
+    bonus numeric,
+    commissionpct float,
+    salesytd float,
+    saleslastyear float,
+    rowguid text,
+    modifiedtimestamp timestamp
+);
+create table "ADVENTUREWORKS"."PUBLIC"."salesterritory"(
+    territoryid numeric primary key,
+    name text,
+    countryregioncode text,
+    group0 text,
+    salesytd float,
+    saleslastyear float,
+    costytd  numeric,
+    costlastyear  numeric,
+    rowguid text,
+    modifiedtimestamp timestamp
+);
